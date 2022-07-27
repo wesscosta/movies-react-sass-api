2eb64465ad18ccbe31ceb229a3898dbaf1cc9ee5
Weslley Costa <weslleycosta.ti@gmail.com>
2022-07-27 03:10:38 -0300

    updade: adicionando css a pagina - styled components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header";
+import { MoviesList } from "./components/MoviesList";
+import { MoviesContainer } from "./styles/MoviesContainer";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+
+      <MoviesContainer>
+        <MoviesList movies={[]}/>
+      </MoviesContainer>
+    </div>
+  );
+}
+
+export default App;
